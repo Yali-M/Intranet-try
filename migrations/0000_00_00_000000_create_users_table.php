6b62783f777933f270ddee062b79294f02e66072
Yali-M <nyali@fgroup-audiovisual.com>
2026-03-07 13:49:01 +0100

    Create migration for users table

---
+++ migrations/0000_00_00_000000_create_users_table.php
@@ -0,0 +1,35 @@
+database/
+│
+├─ migrations/
+│  ├─ 0000_00_00_000000_create_users_table.php
+│  ├─ 0000_00_00_000001_create_roles_table.php
+│  ├─ 0000_00_00_000002_create_permissions_table.php
+│  ├─ 0000_00_00_000003_create_role_user_table.php
+│  ├─ 0000_00_00_000004_create_permission_role_table.php
+│  │
+│  ├─ 2024_01_01_000010_create_boards_table.php
+│  ├─ 2024_01_01_000011_create_columns_table.php
+│  ├─ 2024_01_01_000012_create_tasks_table.php
+│  ├─ 2024_01_01_000013_create_task_comments_table.php
+│  │
+│  ├─ 2024_01_01_000020_create_absences_table.php
+│  ├─ 2024_01_01_000021_create_declarations_table.php
+│  ├─ 2024_01_01_000022_create_documents_table.php
+│  │
+│  ├─ 2024_01_01_000030_create_rewards_table.php
+│  ├─ 2024_01_01_000031_create_point_transactions_table.php
+│  ├─ 2024_01_01_000032_create_reward_orders_table.php
+│  │
+│  ├─ 2024_01_01_000040_create_links_table.php
+│  │
+│  ├─ 2024_01_01_000050_create_activity_logs_table.php
+│  └─ 2024_01_01_000051_create_settings_table.php
+│
+└─ seeders/
+   ├─ DatabaseSeeder.php
+   ├─ RoleSeeder.php
+   ├─ PermissionSeeder.php
+   ├─ AdminUserSeeder.php
+   ├─ KanbanSeeder.php
+   ├─ ValorisSeeder.php
+   └─ SettingsSeeder.php
