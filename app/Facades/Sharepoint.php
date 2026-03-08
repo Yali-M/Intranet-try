990d748d53a4472880877c7d7a70a31c8c1f60c6
Yali-M <nyali@fgroup-audiovisual.com>
2026-03-08 14:42:05 +0100

    Update Sharepoint.php

--- app/Facades/Sharepoint.php
+++ app/Facades/Sharepoint.php
@@ -1,1 +1,11 @@
+namespace App\Facades;
 
+use Illuminate\Support\Facades\Facade;
+
+class Sharepoint extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return 'sharepoint';
+    }
+}
