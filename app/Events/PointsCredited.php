deb8d29010919868475ee34f17a0c3b35f838335
Yali-M <nyali@fgroup-audiovisual.com>
2026-03-08 14:34:23 +0100

    Update PointsCredited.php

--- app/Events/PointsCredited.php
+++ app/Events/PointsCredited.php
@@ -1,1 +1,20 @@
+namespace App\Events;
 
+use App\Models\User;
+use Illuminate\Foundation\Events\Dispatchable;
+
+class PointsCredited
+{
+    use Dispatchable;
+
+    public User $user;
+    public int $points;
+    public string $reason;
+
+    public function __construct(User $user, int $points, string $reason)
+    {
+        $this->user = $user;
+        $this->points = $points;
+        $this->reason = $reason;
+    }
+}
