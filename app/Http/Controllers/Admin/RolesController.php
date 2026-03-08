da80cc4585dea4d485bef86110332eb8ddb2ff6f
Yali-M <nyali@fgroup-audiovisual.com>
2026-03-08 14:44:03 +0100

    Update RolesController.php

--- app/Http/Controllers/Admin/RolesController.php
+++ app/Http/Controllers/Admin/RolesController.php
@@ -1,1 +1,12 @@
+namespace App\Http\Controllers\Admin;
 
+use App\Http\Controllers\Controller;
+use App\Models\Role;
+
+class RolesController extends Controller
+{
+    public function index()
+    {
+        return view('admin.roles.index', ['roles' => Role::all()]);
+    }
+}
