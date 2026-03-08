b3fddae44e05aebd0346104c0b6531b0baab6f14
Yali-M <nyali@fgroup-audiovisual.com>
2026-03-08 14:07:49 +0100

    Update UpdateUserRequest.php

--- app/Http/Requests/Admin/UpdateUserRequest.php
+++ app/Http/Requests/Admin/UpdateUserRequest.php
@@ -1,1 +1,25 @@
+namespace App\Http\Requests\Admin;
 
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdateUserRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return auth()->user()?->isAdmin();
+    }
+
+    public function rules()
+    {
+        return [
+            'name' => ['required','string','max:255'],
+            'email' => [
+                'required',
+                'email',
+                Rule::unique('users','email')->ignore($this->route('user'))
+            ],
+            'password' => ['nullable','string','min:8']
+        ];
+    }
+}
