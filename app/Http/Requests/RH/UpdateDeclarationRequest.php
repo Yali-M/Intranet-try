65836cef33c6270a88cf4797c229377341a5df27
Yali-M <nyali@fgroup-audiovisual.com>
2026-03-08 14:26:58 +0100

    Update UpdateDeclarationRequest.php

--- app/Http/Requests/RH/UpdateDeclarationRequest.php
+++ app/Http/Requests/RH/UpdateDeclarationRequest.php
@@ -1,1 +1,21 @@
+namespace App\Http\Requests\RH;
 
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateDeclarationRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return auth()->check();
+    }
+
+    public function rules()
+    {
+        return [
+            'title' => ['required','string','max:255'],
+            'description' => ['nullable','string'],
+            'participants' => ['required','array'],
+            'participants.*' => ['exists:users,id']
+        ];
+    }
+}
