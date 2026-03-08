bde8f758850a155c0774d3533f76b538b04e9d0e
Yali-M <nyali@fgroup-audiovisual.com>
2026-03-08 14:15:56 +0100

    Update TransferPointsRequest.php

--- app/Http/Requests/Valoris/TransferPointsRequest.php
+++ app/Http/Requests/Valoris/TransferPointsRequest.php
@@ -1,1 +1,20 @@
+namespace App\Http\Requests\Valoris;
 
+use Illuminate\Foundation\Http\FormRequest;
+
+class TransferPointsRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return auth()->check();
+    }
+
+    public function rules()
+    {
+        return [
+            'recipient_id' => ['required','exists:users,id'],
+            'points' => ['required','integer','min:1'],
+            'message' => ['nullable','string']
+        ];
+    }
+}
