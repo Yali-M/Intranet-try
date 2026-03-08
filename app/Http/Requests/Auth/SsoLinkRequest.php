3f42702fdc29769558508b0301fd942d58b73169
Yali-M <nyali@fgroup-audiovisual.com>
2026-03-08 14:05:23 +0100

    Update SsoLinkRequest.php

--- app/Http/Requests/Auth/SsoLinkRequest.php
+++ app/Http/Requests/Auth/SsoLinkRequest.php
@@ -1,1 +1,20 @@
+namespace App\Http\Requests\Auth;
 
+use Illuminate\Foundation\Http\FormRequest;
+
+class SsoLinkRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return auth()->check();
+    }
+
+    public function rules()
+    {
+        return [
+            'provider' => ['required','in:azure,keycloak'],
+            'provider_id' => ['required','string'],
+            'email' => ['required','email']
+        ];
+    }
+}
