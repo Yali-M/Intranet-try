e3a7f1b46569818e398b54039211a9b72bae9cf2
Yali-M <nyali@fgroup-audiovisual.com>
2026-03-08 15:05:54 +0100

    Update OutilsApiMiddleware.php

--- app/Http/Middleware/OutilsApiMiddleware.php
+++ app/Http/Middleware/OutilsApiMiddleware.php
@@ -1,1 +1,18 @@
+namespace App\Http\Middleware;
 
+use Closure;
+use Illuminate\Http\Request;
+
+class OutilsApiMiddleware
+{
+    public function handle(Request $request, Closure $next)
+    {
+        $token = $request->header('X-API-TOKEN');
+
+        if (!$token || $token !== config('intranet.outils_api_token')) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return $next($request);
+    }
+}
