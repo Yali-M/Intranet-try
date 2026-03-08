38ba4721b635e96415197b8736dd93a2a3a430e8
Yali-M <nyali@fgroup-audiovisual.com>
2026-03-08 14:48:46 +0100

    Update KanbanApiController.php

--- app/Http/Controllers/Api/KanbanApiController.php
+++ app/Http/Controllers/Api/KanbanApiController.php
@@ -1,1 +1,14 @@
+namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Kanban\CreateTaskRequest;
+use App\Models\KanboardTache;
+
+class KanbanApiController extends Controller
+{
+    public function storeTask(CreateTaskRequest $request)
+    {
+        $task = KanboardTache::create($request->validated());
+        return response()->json($task, 201);
+    }
+}
