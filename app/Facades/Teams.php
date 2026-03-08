75a9e8b894cad26badc2975f0942d405b93aed3b
Yali-M <nyali@fgroup-audiovisual.com>
2026-03-08 14:41:18 +0100

    Update Teams.php

--- app/Facades/Teams.php
+++ app/Facades/Teams.php
@@ -1,1 +1,11 @@
+namespace App\Facades;
 
+use Illuminate\Support\Facades\Facade;
+
+class Teams extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return 'teams';
+    }
+}
