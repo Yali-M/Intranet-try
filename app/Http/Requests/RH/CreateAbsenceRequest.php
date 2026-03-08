abcaeadb1c39c67a4859cd80d91a318eccca06ad
Yali-M <nyali@fgroup-audiovisual.com>
2026-03-08 14:25:14 +0100

    Update CreateAbsenceRequest.php

--- app/Http/Requests/RH/CreateAbsenceRequest.php
+++ app/Http/Requests/RH/CreateAbsenceRequest.php
@@ -1,1 +1,21 @@
+namespace App\Http\Requests\RH;
 
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateAbsenceRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return auth()->check();
+    }
+
+    public function rules()
+    {
+        return [
+            'type' => ['required','string'],                   // Exemple : "congé", "maladie"
+            'start_date' => ['required','date'],              // Date de début
+            'end_date' => ['required','date','after_or_equal:start_date'], // Date de fin
+            'comment' => ['nullable','string','max:500']
+        ];
+    }
+}
