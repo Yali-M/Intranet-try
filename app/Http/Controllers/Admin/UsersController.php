b72c92a15f2c71c36437e89fd9972ff158c34664
Yali-M <nyali@fgroup-audiovisual.com>
2026-03-08 14:43:31 +0100

    Update UsersController.php

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -1,1 +1,26 @@
+namespace App\Http\Controllers\Admin;
 
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Http\Requests\Admin\CreateUserRequest;
+use App\Http\Requests\Admin\UpdateUserRequest;
+
+class UsersController extends Controller
+{
+    public function index()
+    {
+        return view('admin.users.index', ['users' => User::all()]);
+    }
+
+    public function store(CreateUserRequest $request)
+    {
+        User::create($request->validated());
+        return redirect()->back()->with('success', 'User created!');
+    }
+
+    public function update(UpdateUserRequest $request, User $user)
+    {
+        $user->update($request->validated());
+        return redirect()->back()->with('success', 'User updated!');
+    }
+}
