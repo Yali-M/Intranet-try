e6b4e439fdf3b2768ccf99d54c1a85c800e40539
Yali-M <nyali@fgroup-audiovisual.com>
2026-03-08 15:45:52 +0100

    Update services.php

--- config/services.php
+++ config/services.php
@@ -1,1 +1,29 @@
 
+return [
+
+    'azure' => [
+        'client_id' => env('AZURE_CLIENT_ID'),
+        'client_secret' => env('AZURE_CLIENT_SECRET'),
+        'tenant_id' => env('AZURE_TENANT_ID'),
+        'redirect' => env('AZURE_REDIRECT_URI'),
+    ],
+
+    'keycloak' => [
+        'base_url' => env('KEYCLOAK_BASE_URL'),
+        'realm' => env('KEYCLOAK_REALM'),
+        'client_id' => env('KEYCLOAK_CLIENT_ID'),
+        'client_secret' => env('KEYCLOAK_CLIENT_SECRET'),
+        'redirect' => env('KEYCLOAK_REDIRECT_URI'),
+    ],
+
+    'teams' => [
+        'webhook_url' => env('TEAMS_WEBHOOK_URL'),
+    ],
+
+    'sharepoint' => [
+        'site_url' => env('SHAREPOINT_SITE_URL'),
+        'client_id' => env('SHAREPOINT_CLIENT_ID'),
+        'client_secret' => env('SHAREPOINT_CLIENT_SECRET'),
+    ],
+
+];
