d2b90fda59d051ac8b3206a6db94bb8e3b29ce35
Yali-M <nyali@fgroup-audiovisual.com>
2026-03-08 14:21:38 +0100

    Update ValorisHistory.php

--- app/Models/ValorisHistory.php
+++ app/Models/ValorisHistory.php
@@ -1,1 +1,18 @@
+namespace App\Models;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class ValorisHistory extends Model
+{
+    protected $table = 'valoris_history';
+
+    protected $fillable = [
+        'user_id', 'points', 'type', 'reason', 'related_id'
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
