a586f8cd00f84020008ab61d729b8ad7740da1b3
Yali-M <nyali@fgroup-audiovisual.com>
2026-03-08 14:14:01 +0100

    Update CreateRewardRequest.php

--- app/Http/Requests/Valoris/CreateRewardRequest.php
+++ app/Http/Requests/Valoris/CreateRewardRequest.php
@@ -1,1 +1,21 @@
+namespace App\Http\Requests\Valoris;
 
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateRewardRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return auth()->user()?->isAdmin();
+    }
+
+    public function rules()
+    {
+        return [
+            'name' => ['required','string','max:255'],
+            'description' => ['nullable','string'],
+            'points_cost' => ['required','integer','min:1'],
+            'stock' => ['required','integer','min:0']
+        ];
+    }
+}
