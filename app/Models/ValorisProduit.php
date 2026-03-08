1c24bb96b0cf977e04d82235e133938b217ff234
Yali-M <nyali@fgroup-audiovisual.com>
2026-03-08 14:20:48 +0100

    Update ValorisProduit.php

--- app/Models/ValorisProduit.php
+++ app/Models/ValorisProduit.php
@@ -1,1 +1,18 @@
+namespace App\Models;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class ValorisProduit extends Model
+{
+    protected $table = 'valoris_produits';
+
+    protected $fillable = [
+        'name', 'description', 'points_cost', 'stock'
+    ];
+
+    public function achats(): HasMany
+    {
+        return $this->hasMany(ValorisAchat::class, 'reward_id');
+    }
+}
