829721e6481843264ec8c41a93960a63995b929c
Yali-M <nyali@fgroup-audiovisual.com>
2026-03-08 14:15:02 +0100

    Update PurchaseRewardRequest.php

--- app/Http/Requests/Valoris/PurchaseRewardRequest.php
+++ app/Http/Requests/Valoris/PurchaseRewardRequest.php
@@ -1,1 +1,18 @@
+namespace App\Http\Requests\Valoris;
 
+use Illuminate\Foundation\Http\FormRequest;
+
+class PurchaseRewardRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return auth()->check();
+    }
+
+    public function rules()
+    {
+        return [
+            'reward_id' => ['required','exists:rewards,id']
+        ];
+    }
+}
