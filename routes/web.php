0bd0328372433e379084cbbb1fde76b2a6a1d0aa
Yali-M <nyali@fgroup-audiovisual.com>
2026-03-08 15:27:46 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,1 +1,27 @@
+use App\Http\Controllers\KanbanController;
+use App\Http\Controllers\ValorisController;
+use App\Http\Controllers\OutilsController;
+use Illuminate\Support\Facades\Route;
 
+// Routes publiques
+Route::get('/login', [\App\Http\Controllers\Auth\LoginController::class, 'showLoginForm'])->name('login');
+
+// Routes protégées par connexion
+Route::middleware(['connected'])->group(function () {
+
+    // Dashboard Kanban
+    Route::get('/kanban', [KanbanController::class, 'index']);
+    Route::post('/kanban/task', [KanbanController::class, 'storeTask']);
+    Route::put('/kanban/task/{task}', [KanbanController::class, 'updateTask']);
+    Route::post('/kanban/task/move', [KanbanController::class, 'moveTask']);
+
+    // Valoris
+    Route::get('/valoris/catalog', [ValorisController::class, 'catalog']);
+    Route::post('/valoris/purchase', [ValorisController::class, 'purchase']);
+    Route::get('/valoris/balance', [ValorisController::class, 'pointsBalance']);
+
+    // Outils intranet
+    Route::get('/outils', [OutilsController::class, 'index']);
+    Route::post('/outils/link', [OutilsController::class, 'store']);
+    Route::put('/outils/link/{link}', [OutilsController::class, 'update']);
+});
