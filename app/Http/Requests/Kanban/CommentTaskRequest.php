bd9f99ad44324bb486a03f71d3e9d055110a6b48
Yali-M <nyali@fgroup-audiovisual.com>
2026-03-08 13:52:33 +0100

    Update CommentTaskRequest.php

--- app/Http/Requests/Kanban/CommentTaskRequest.php
+++ app/Http/Requests/Kanban/CommentTaskRequest.php
@@ -1,1 +1,19 @@
+namespace App\Http\Requests\Kanban;
 
+use Illuminate\Foundation\Http\FormRequest;
+
+class CommentTaskRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return auth()->check();
+    }
+
+    public function rules()
+    {
+        return [
+            'task_id' => ['required','exists:tasks,id'],
+            'comment' => ['required','string','max:2000']
+        ];
+    }
+}
