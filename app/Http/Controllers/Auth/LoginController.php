9b51eff3a27cdfefa2ddb8fd5e6bf15516b6bb80
Yali-M <nyali@fgroup-audiovisual.com>
2026-03-08 15:00:42 +0100

    Update LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -1,1 +1,13 @@
+namespace App\Http\Controllers\Auth;
 
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\AzureLoginRequest;
+
+class LoginController extends Controller
+{
+    public function azureLogin(AzureLoginRequest $request)
+    {
+        $token = $request->token;
+        // Appel service Azure pour authentification
+    }
+}
