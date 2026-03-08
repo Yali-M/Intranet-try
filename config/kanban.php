3b296030a623fac2f483daec51de8142b41ca062
Yali-M <nyali@fgroup-audiovisual.com>
2026-03-08 15:46:51 +0100

    Update kanban.php

--- config/kanban.php
+++ config/kanban.php
@@ -1,1 +1,9 @@
-
+return [
+    'default_columns' => ['À faire', 'En cours', 'Terminé'],
+    'column_colors' => [
+        'À faire' => '#f39c12',
+        'En cours' => '#3498db',
+        'Terminé' => '#2ecc71',
+    ],
+    'max_tasks_per_column' => 50,
+];
