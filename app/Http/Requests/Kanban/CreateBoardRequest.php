5276801a84d3a21cd9aec2decbe2d06739af79d5
Yali-M <nyali@fgroup-audiovisual.com>
2026-03-08 13:53:18 +0100

    Update CreateBoardRequest.php

--- app/Http/Requests/Kanban/CreateBoardRequest.php
+++ app/Http/Requests/Kanban/CreateBoardRequest.php
@@ -1,1 +1,20 @@
+namespace App\Http\Requests\Kanban;
 
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateBoardRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return auth()->check();
+    }
+
+    public function rules()
+    {
+        return [
+            'name' => ['required','string','max:255'],
+            'description' => ['nullable','string','max:1000'],
+            'visibility' => ['required','in:private,team,public']
+        ];
+    }
+}
