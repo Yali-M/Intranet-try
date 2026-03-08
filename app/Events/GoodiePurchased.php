6fdbb994aae3dcb596d0e064ecbab1f2fe8949ea
Yali-M <nyali@fgroup-audiovisual.com>
2026-03-08 14:34:56 +0100

    Update GoodiePurchased.php

--- app/Events/GoodiePurchased.php
+++ app/Events/GoodiePurchased.php
@@ -1,1 +1,16 @@
+namespace App\Events;
 
+use App\Models\ValorisAchat;
+use Illuminate\Foundation\Events\Dispatchable;
+
+class GoodiePurchased
+{
+    use Dispatchable;
+
+    public ValorisAchat $achat;
+
+    public function __construct(ValorisAchat $achat)
+    {
+        $this->achat = $achat;
+    }
+}
