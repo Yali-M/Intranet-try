99463d3f4b8d91810eddf88f6f58255f8acbe5eb
Yali-M <nyali@fgroup-audiovisual.com>
2026-03-08 15:28:07 +0100

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,1 +1,20 @@
+use App\Http\Controllers\Api\KanbanApiController;
+use App\Http\Controllers\Api\ValorisApiController;
+use App\Http\Controllers\Api\RhApiController;
+use Illuminate\Support\Facades\Route;
 
+// Middleware spécifique API (auth token, CORS)
+Route::middleware(['auth:sanctum'])->group(function() {
+
+    // Kanban API
+    Route::get('/kanban/tasks', [KanbanApiController::class, 'index']);
+    Route::post('/kanban/tasks', [KanbanApiController::class, 'storeTask']);
+
+    // Valoris API
+    Route::post('/valoris/purchase', [ValorisApiController::class, 'purchase']);
+    Route::get('/valoris/points', [ValorisApiController::class, 'pointsBalance']);
+
+    // RH API
+    Route::get('/rh/absences', [RhApiController::class, 'index']);
+    Route::post('/rh/absences', [RhApiController::class, 'store']);
+});
