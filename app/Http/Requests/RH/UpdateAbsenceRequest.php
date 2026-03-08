9f4e40418d7c158bb995d1e0e40b9a558672cf64
Yali-M <nyali@fgroup-audiovisual.com>
2026-03-08 14:25:40 +0100

    Update UpdateAbsenceRequest.php

--- app/Http/Requests/RH/UpdateAbsenceRequest.php
+++ app/Http/Requests/RH/UpdateAbsenceRequest.php
@@ -1,1 +1,21 @@
+namespace App\Http\Requests\RH;
 
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateAbsenceRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return auth()->check();
+    }
+
+    public function rules()
+    {
+        return [
+            'type' => ['required','string'],
+            'start_date' => ['required','date'],
+            'end_date' => ['required','date','after_or_equal:start_date'],
+            'comment' => ['nullable','string','max:500']
+        ];
+    }
+}
