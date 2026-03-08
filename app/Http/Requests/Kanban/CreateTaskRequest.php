1650b0f3fe1aec999d5e2b5d9629dd4c9793e6ca
Yali-M <nyali@fgroup-audiovisual.com>
2026-03-08 13:54:09 +0100

    Update CreateTaskRequest.php

--- app/Http/Requests/Kanban/CreateTaskRequest.php
+++ app/Http/Requests/Kanban/CreateTaskRequest.php
@@ -1,1 +1,22 @@
+namespace App\Http\Requests\Kanban;
 
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateTaskRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return auth()->check();
+    }
+
+    public function rules()
+    {
+        return [
+            'title' => ['required','string','max:255'],
+            'description' => ['nullable','string'],
+            'board_id' => ['required','exists:kanboards,id'],
+            'assigned_to' => ['nullable','exists:users,id'],
+            'due_date' => ['nullable','date']
+        ];
+    }
+}
