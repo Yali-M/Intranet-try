50b9a9566462bbd31c76d377937f11502f6023c4
Yali-M <nyali@fgroup-audiovisual.com>
2026-03-08 15:03:36 +0100

    Update SsoCallbackController.php

--- app/Http/Controllers/Auth/SsoCallbackController.php
+++ app/Http/Controllers/Auth/SsoCallbackController.php
@@ -1,1 +1,32 @@
+namespace App\Http\Controllers\Auth;
 
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Services\Auth\AzureService;
+use App\Services\Auth\KeycloakService;
+
+class SsoCallbackController extends Controller
+{
+    protected AzureService $azureService;
+    protected KeycloakService $keycloakService;
+
+    public function __construct(AzureService $azureService, KeycloakService $keycloakService)
+    {
+        $this->azureService = $azureService;
+        $this->keycloakService = $keycloakService;
+    }
+
+    public function handleAzure(Request $request)
+    {
+        $user = $this->azureService->handleCallback($request->code);
+        auth()->login($user);
+        return redirect()->intended('/');
+    }
+
+    public function handleKeycloak(Request $request)
+    {
+        $user = $this->keycloakService->handleCallback($request->code);
+        auth()->login($user);
+        return redirect()->intended('/');
+    }
+}
