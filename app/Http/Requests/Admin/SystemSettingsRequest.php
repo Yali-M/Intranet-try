aaa502aabf0c404be5ae8457fe1c14ecf984293c
Yali-M <nyali@fgroup-audiovisual.com>
2026-03-08 14:09:19 +0100

    Update SystemSettingsRequest.php

--- app/Http/Requests/Admin/SystemSettingsRequest.php
+++ app/Http/Requests/Admin/SystemSettingsRequest.php
@@ -1,1 +1,21 @@
+namespace App\Http\Requests\Admin;
 
+use Illuminate\Foundation\Http\FormRequest;
+
+class SystemSettingsRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return auth()->user()?->isAdmin();
+    }
+
+    public function rules()
+    {
+        return [
+            'app_name' => ['required','string','max:255'],
+            'timezone' => ['required','string'],
+            'support_email' => ['required','email'],
+            'maintenance_mode' => ['boolean']
+        ];
+    }
+}
