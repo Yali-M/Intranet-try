03412468afb8b145503f05842330b94a83285d42
Yali-M <nyali@fgroup-audiovisual.com>
2026-03-08 14:17:16 +0100

    Update Kanboard.php

--- app/Models/Kanboard.php
+++ app/Models/Kanboard.php
@@ -1,1 +1,26 @@
+namespace App\Models;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Kanboard extends Model
+{
+    protected $table = 'kanboards';
+
+    protected $fillable = [
+        'name',
+        'description',
+        'owner_id'
+    ];
+
+    // Relations
+    public function colonnes(): HasMany
+    {
+        return $this->hasMany(KanboardColonne::class, 'kanboard_id');
+    }
+
+    public function taches(): HasMany
+    {
+        return $this->hasManyThrough(KanboardTache::class, KanboardColonne::class, 'kanboard_id', 'colonne_id');
+    }
+}
