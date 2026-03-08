6a27f6c7e1b4d040f20745e672c96ef42ad98742
Yali-M <nyali@fgroup-audiovisual.com>
2026-03-08 14:08:51 +0100

    Update UpdatePermissionRequest.php

--- app/Http/Requests/Admin/UpdatePermissionRequest.php
+++ app/Http/Requests/Admin/UpdatePermissionRequest.php
@@ -1,1 +1,20 @@
+namespace App\Http\Requests\Admin;
 
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdatePermissionRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return auth()->user()?->isAdmin();
+    }
+
+    public function rules()
+    {
+        return [
+            'role' => ['required','string','exists:roles,name'],
+            'permissions' => ['required','array'],
+            'permissions.*' => ['string','exists:permissions,name']
+        ];
+    }
+}
