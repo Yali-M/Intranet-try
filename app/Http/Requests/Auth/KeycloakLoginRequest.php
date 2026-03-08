4a29c051b2d112aea6f44a01ac30b4ec60594635
Yali-M <nyali@fgroup-audiovisual.com>
2026-03-08 14:04:51 +0100

    Update KeycloakLoginRequest.php

--- app/Http/Requests/Auth/KeycloakLoginRequest.php
+++ app/Http/Requests/Auth/KeycloakLoginRequest.php
@@ -1,1 +1,20 @@
+namespace App\Http\Requests\Auth;
 
+use Illuminate\Foundation\Http\FormRequest;
+
+class KeycloakLoginRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'code' => ['required','string'],
+            'state' => ['required','string'],
+            'session_state' => ['nullable','string']
+        ];
+    }
+}
