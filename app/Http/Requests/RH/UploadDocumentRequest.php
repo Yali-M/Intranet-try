a8b8e25017b52cae320c07a9e44369fa42ae10aa
Yali-M <nyali@fgroup-audiovisual.com>
2026-03-08 14:27:33 +0100

    Update UploadDocumentRequest.php

--- app/Http/Requests/RH/UploadDocumentRequest.php
+++ app/Http/Requests/RH/UploadDocumentRequest.php
@@ -1,1 +1,19 @@
+namespace App\Http\Requests\RH;
 
+use Illuminate\Foundation\Http\FormRequest;
+
+class UploadDocumentRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return auth()->check();
+    }
+
+    public function rules()
+    {
+        return [
+            'file' => ['required','file','max:10240'], // Max 10 MB
+            'category' => ['required','string']       // Exemple : "contrat", "fiche de paie"
+        ];
+    }
+}
