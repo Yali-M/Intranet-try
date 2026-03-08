ca6f7e87424d59170f42b2fd78a9997935aea57c
Yali-M <nyali@fgroup-audiovisual.com>
2026-03-08 21:15:17 +0100

    Update PermissionSeeder.php

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -1,1 +1,41 @@
+<?php
 
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+
+class PermissionSeeder extends Seeder
+{
+    public function run(): void
+    {
+
+        $permissions = [
+
+            'Absences.gestion',
+            'Absences.validate',
+            'users.get',
+            'Valoris.addpoints',
+            'Equipe.edit',
+            'Equipe.delete',
+            'Emissions.all',
+            'Emissions.ForceEdit',
+            'Emissions.ForceDelete',
+            'Emissions.validate',
+            'Episode.remove',
+            'valoris-declaration-mensuel',
+            'valoris-declaration-mensuel-force',
+            'valoris-declaration-mensuel-list'
+
+        ];
+
+        foreach ($permissions as $permission) {
+
+            Permission::firstOrCreate([
+                'name' => $permission
+            ]);
+
+        }
+
+    }
+}
