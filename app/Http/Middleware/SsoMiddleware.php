cad6e911094073eb82dbc82975542aed271dcffd
Yali-M <nyali@fgroup-audiovisual.com>
2026-03-08 15:04:59 +0100

    Update SsoMiddleware.php

--- app/Http/Middleware/SsoMiddleware.php
+++ app/Http/Middleware/SsoMiddleware.php
@@ -1,1 +1,16 @@
+namespace App\Http\Middleware;
 
+use Closure;
+use Illuminate\Http\Request;
+
+class SsoMiddleware
+{
+    public function handle(Request $request, Closure $next)
+    {
+        if (!session()->has('sso_authenticated')) {
+            return redirect()->route('login');
+        }
+
+        return $next($request);
+    }
+}
