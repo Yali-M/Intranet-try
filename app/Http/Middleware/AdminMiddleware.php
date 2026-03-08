6cec5f06cea0c9d56045c03021c091515b937b24
Yali-M <nyali@fgroup-audiovisual.com>
2026-03-08 15:05:24 +0100

    Update AdminMiddleware.php

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -1,1 +1,16 @@
+namespace App\Http\Middleware;
 
+use Closure;
+use Illuminate\Http\Request;
+
+class AdminMiddleware
+{
+    public function handle(Request $request, Closure $next)
+    {
+        if (!auth()->check() || !auth()->user()->is_admin) {
+            abort(403, 'Accès interdit');
+        }
+
+        return $next($request);
+    }
+}
