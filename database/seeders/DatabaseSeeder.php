88a79a1efc11d1c9a1e1c47ef1f7e40826d79eaa
Yali-M <nyali@fgroup-audiovisual.com>
2026-03-08 21:14:55 +0100

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,1 +1,22 @@
+<?php
 
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    public function run(): void
+    {
+
+        $this->call([
+            PermissionSeeder::class,
+            RoleSeeder::class,
+            AdminUserSeeder::class,
+            EquipeSeeder::class,
+            EmissionSeeder::class,
+            NotificationSeeder::class
+        ]);
+
+    }
+}
