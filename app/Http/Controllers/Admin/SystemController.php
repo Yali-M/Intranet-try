fabcaa675908353e069f8a7aca955904a011fe6f
Yali-M <nyali@fgroup-audiovisual.com>
2026-03-08 14:45:02 +0100

    Update SystemController.php

--- app/Http/Controllers/Admin/SystemController.php
+++ app/Http/Controllers/Admin/SystemController.php
@@ -1,1 +1,13 @@
+namespace App\Http\Controllers\Admin;
 
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\SystemSettingsRequest;
+
+class SystemController extends Controller
+{
+    public function update(SystemSettingsRequest $request)
+    {
+        config(['intranet.settings' => $request->validated()]);
+        return redirect()->back()->with('success', 'Settings updated');
+    }
+}
