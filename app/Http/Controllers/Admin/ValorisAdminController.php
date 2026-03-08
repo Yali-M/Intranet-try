587d4b0a33b7959631b3c51285dfd60a0f89edff
Yali-M <nyali@fgroup-audiovisual.com>
2026-03-08 14:44:32 +0100

    Update ValorisAdminController.php

--- app/Http/Controllers/Admin/ValorisAdminController.php
+++ app/Http/Controllers/Admin/ValorisAdminController.php
@@ -1,1 +1,12 @@
+namespace App\Http\Controllers\Admin;
 
+use App\Http\Controllers\Controller;
+use App\Models\ValorisProduit;
+
+class ValorisAdminController extends Controller
+{
+    public function products()
+    {
+        return view('admin.valoris.products', ['products' => ValorisProduit::all()]);
+    }
+}
