ac9628073cdd7bebf5a9d8ff61d4f2064bb63fda
Yali-M <nyali@fgroup-audiovisual.com>
2026-03-08 14:15:30 +0100

    Update GrantPointsRequest.php

--- app/Http/Requests/Valoris/GrantPointsRequest.php
+++ app/Http/Requests/Valoris/GrantPointsRequest.php
@@ -1,1 +1,20 @@
+namespace App\Http\Requests\Valoris;
 
+use Illuminate\Foundation\Http\FormRequest;
+
+class GrantPointsRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return auth()->user()?->isManager() || auth()->user()?->isAdmin();
+    }
+
+    public function rules()
+    {
+        return [
+            'user_id' => ['required','exists:users,id'],
+            'points' => ['required','integer','min:1'],
+            'reason' => ['nullable','string']
+        ];
+    }
+}
