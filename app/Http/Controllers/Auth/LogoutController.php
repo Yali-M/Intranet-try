1586c22ded7ac85b871a0d8022bd6c13480d3b55
Yali-M <nyali@fgroup-audiovisual.com>
2026-03-08 15:03:11 +0100

    Update LogoutController.php

--- app/Http/Controllers/Auth/LogoutController.php
+++ app/Http/Controllers/Auth/LogoutController.php
@@ -1,1 +1,16 @@
+namespace App\Http\Controllers\Auth;
 
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LogoutController extends Controller
+{
+    public function logout(Request $request)
+    {
+        auth()->logout();               // Déconnexion Laravel
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/login');      // Redirection vers login
+    }
+}
