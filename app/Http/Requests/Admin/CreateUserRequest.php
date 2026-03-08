fe2dd72c81dcc24317e411527630c1dfeedaf421
Yali-M <nyali@fgroup-audiovisual.com>
2026-03-08 14:07:07 +0100

    Update CreateUserRequest.php

--- app/Http/Requests/Admin/CreateUserRequest.php
+++ app/Http/Requests/Admin/CreateUserRequest.php
@@ -1,1 +1,21 @@
+namespace App\Http\Requests\Admin;
 
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateUserRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return auth()->user()?->isAdmin();
+    }
+
+    public function rules()
+    {
+        return [
+            'name' => ['required','string','max:255'],
+            'email' => ['required','email','unique:users,email'],
+            'password' => ['required','string','min:8'],
+            'role' => ['required','string']
+        ];
+    }
+}
