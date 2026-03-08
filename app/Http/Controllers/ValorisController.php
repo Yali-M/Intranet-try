f75a9e9f909c8ce0d5358ebaef3a29241172f454
Yali-M <nyali@fgroup-audiovisual.com>
2026-03-08 15:08:17 +0100

    Update ValorisController.php

--- app/Http/Controllers/ValorisController.php
+++ app/Http/Controllers/ValorisController.php
@@ -1,1 +1,34 @@
+namespace App\Http\Controllers;
 
+use App\Models\ValorisProduit;
+use App\Http\Requests\Valoris\PurchaseRewardRequest;
+use App\Services\ShopService;
+use Illuminate\Http\Request;
+
+class ValorisController extends Controller
+{
+    protected ShopService $shopService;
+
+    public function __construct(ShopService $shopService)
+    {
+        $this->shopService = $shopService;
+    }
+
+    public function catalog()
+    {
+        $products = ValorisProduit::all();
+        return view('valoris.catalog', compact('products'));
+    }
+
+    public function purchase(PurchaseRewardRequest $request)
+    {
+        $order = $this->shopService->purchase(auth()->user(), $request->reward_id);
+        return redirect()->back()->with('success', 'Récompense achetée !');
+    }
+
+    public function pointsBalance()
+    {
+        $points = auth()->user()->pointsBalance();
+        return view('valoris.balance', compact('points'));
+    }
+}
