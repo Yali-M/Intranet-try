bfcaa2803e6d7cebee1eafa7dbd68d3ff8ab7c42
Yali-M <nyali@fgroup-audiovisual.com>
2026-03-08 21:15:44 +0100

    Update RoleSeeder.php

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -1,1 +1,21 @@
+<?php
 
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class RoleSeeder extends Seeder
+{
+    public function run(): void
+    {
+
+        $admin = Role::firstOrCreate(['name' => 'Admin']);
+        $rh = Role::firstOrCreate(['name' => 'RH']);
+        $responsable = Role::firstOrCreate(['name' => 'Responsable Equipe']);
+
+        $admin->givePermissionTo(Permission::all());
+
+    }
+}
