a1a0e9c88a85027d7b628a1dbadb1b3ae70d33df
Yali-M <nyali@fgroup-audiovisual.com>
2026-03-08 15:47:14 +0100

    Update intranet.php

--- config/intranet.php
+++ config/intranet.php
@@ -1,1 +1,7 @@
-
+return [
+    'app_name' => env('APP_NAME', 'Intranet'),
+    'default_language' => 'fr',
+    'timezone' => 'Europe/Paris',
+    'upload_max_size_mb' => 10,
+    'support_email' => 'support@entreprise.com',
+];
